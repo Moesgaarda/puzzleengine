144af7ab747ab957361edd5b68de4b42bbaf0604
Daniel Moesgaard <moesgaarda@gmail.com>
2020-04-18 18:19:46 +0200

    work before dinner

--- reachability.hpp
+++ reachability.hpp
@@ -20,27 +20,27 @@ enum search_order_t {
 };
 
 // Pass the transition generator function
-template<class StateT, template<class...> class ContainerT>
-function<ContainerT<function<void(StateT &)>>(StateT &)>
-successors(ContainerT<function<void(StateT &)>> (*transitions)(const StateT &)) {
+template<class state_t, template<class...> class container_t>
+function<container_t<function<void(state_t &)>>(state_t &)>
+successors(container_t<function<void(state_t &)>> (*transitions)(const state_t &)) {
     return transitions;
 }
 
-// Keep the trace
-template<class StateT>
+// Keep the trace with a copy of self and a pointer to parent.
+template<class state_t>
 struct trace_state {
     trace_state *parent = nullptr;
-    StateT self = nullptr;
+    state_t self = nullptr;
 };
 
 // Log function to print output, used in family.cpp instead of cout.
 void log(string input) {
     cout << input << endl;
-};
+}
 
 // Overload of << operator to print list content
-template<class StateT>
-ostream &operator<<(ostream &os, const vector<StateT> &v) {
+template<class state_t>
+ostream &operator<<(ostream &os, const vector<state_t> &v) {
     for (auto var : v) {
         os << (int) var;
     }
@@ -48,8 +48,8 @@ ostream &operator<<(ostream &os, const vector<StateT> &v) {
 }
 
 // Overload of << operator to print array content
-template<typename StateT>
-ostream &operator<<(ostream &os, const array<StateT, 3> &v) {
+template<typename state_t>
+ostream &operator<<(ostream &os, const array<state_t, 3> &v) {
     for (auto arr : v) {
         os << (int) arr;
     }
@@ -58,53 +58,80 @@ ostream &operator<<(ostream &os, const array<StateT, 3> &v) {
 
 
 // The state space class
-template<class StateT, template<class...> class ContainerT>
+template<class state_t, template<class...> class container_t, class cost_t = std::nullptr_t>
 class state_space_t {
 private:
-    StateT _initialState; // Initial state
-    function<ContainerT<function<void(StateT &)>>(StateT &)> _transitionFunction;
-    function<bool(const StateT &)> _invariantFunction;
+    state_t _initialState; // Initial state
+    cost_t _initialCost;
+    function<container_t<function<void(state_t &)>>(state_t &)> _transitionFunction;
+    function<bool(const state_t &)> _invariantFunction;
+    bool _useCost = false;
+    function<cost_t(const state_t &state, const cost_t &cost)> _costFunction;
+    template<class validation_f>
+    container_t<state_t> solver(validation_f isGoalState, search_order_t searchOrder);
 
-    template<class ValidationFunction>
-    ContainerT<StateT> solver(ValidationFunction isGoalState, search_order_t searchOrder);
+    template<class validation_f>
+    container_t<state_t> costSolver(validation_f isGoalState);
 
 
 public:
     // Default constructor with no cost
     state_space_t(
-            StateT initialState,
-            function<ContainerT<function<void(StateT &)>>(StateT &)> transitionFunction,
-            bool invariantFunc(const StateT &) = [](
-                    const StateT &state) { return true; } // Default value is a function that takes a const state and returns true.
+            const state_t initialState,
+            function<container_t<function<void(state_t &)>>(state_t &)> transitionFunction,
+            bool invariantFunction(const state_t&) = [](
+                    const state_t &state) { return true; } // Default value is a function that takes a const state and returns true.
     ) {
         _initialState = initialState;
         _transitionFunction = transitionFunction;
-        _invariantFunction = invariantFunc;
+        _invariantFunction = invariantFunction;
+        _useCost = false;
+    };
+
+    // Constructor with cost enabled
+    state_space_t(
+            const state_t initialState,
+            const cost_t initialCost,
+            function<container_t<function<void(state_t &)>>(state_t &)> transitionFunction,
+            bool (*invariantFunction)(const state_t &) = [](const state_t &s) { return true; },
+            function<cost_t(const state_t &s, const cost_t &c)> costFunction = [](
+                    const state_t &s, const cost_t &c) { return cost_t{0, 0}; }
+    ) {
+        _initialState = initialState;
+        _initialCost = initialCost;
+        _transitionFunction = transitionFunction;
+        _invariantFunction = invariantFunction;
+        _useCost = true;
+        _costFunction = costFunction;
     }
 
     // The function to call the solver, default search order is breadth_first, as a sensible choice as defined in
     // requirement 8.
-    template<class ValidationFunction>
-    ContainerT<StateT> check(
-            ValidationFunction isGoalState,
+    template<class validation_f>
+    container_t<state_t> check(
+            validation_f isGoalState,
             search_order_t order = search_order_t::breadth_first) {
+
+        if(_useCost){
+            return costSolver(isGoalState);
+        }
         return solver(isGoalState, order);
     }
 };
 
-template<class StateT, template<class...> class ContainerT>
-template<class ValidationFunction>
-ContainerT<StateT>
-state_space_t<StateT, ContainerT>::solver(ValidationFunction isGoalState, search_order_t order) {
-    StateT currentState;
-    trace_state<StateT> *traceState;
-    list<StateT> passed;
-    list<trace_state<StateT> *> waiting;
-    list<StateT> solution;
-    ContainerT<StateT> containedSolution;
+template<class state_t, template<class...> class container_t, class cost_t>
+template<class validation_f>
+container_t<state_t>
+state_space_t<state_t, container_t, cost_t>::solver(validation_f isGoalState, search_order_t order) {
+    state_t currentState;
+    trace_state<state_t> *traceState{};
+    list<state_t> passed;
+    list<trace_state<state_t> *> waiting;
+    list<state_t> solution;
+    container_t<state_t> containedSolution;
 
     // Adding the states to waiting
-    waiting.push_back(new trace_state<StateT>{nullptr, _initialState});
+    waiting.push_back(new trace_state<state_t>{nullptr, _initialState});
 
     // Keep iterating through the waiting list until it is empty
     while (!waiting.empty()) {
@@ -131,7 +158,7 @@ state_space_t<StateT, ContainerT>::solver(ValidationFunction isGoalState, search
 
 
             // Convert to a vector
-            for (StateT &st: solution) {
+            for (state_t &st: solution) {
                 containedSolution.push_back(st);
             }
 
@@ -147,7 +174,7 @@ state_space_t<StateT, ContainerT>::solver(ValidationFunction isGoalState, search
                 transition(successor);
 
                 if (_invariantFunction(successor)) {
-                    waiting.push_back(new trace_state<StateT>{traceState, successor});
+                    waiting.push_back(new trace_state<state_t>{traceState, successor});
                 }
             }
         }
@@ -155,5 +182,73 @@ state_space_t<StateT, ContainerT>::solver(ValidationFunction isGoalState, search
     return containedSolution;
 }
 
+template<class state_t, template<class...> class container_t, class cost_t>
+template<class validation_f>
+container_t<state_t>
+state_space_t<state_t, container_t, cost_t>::costSolver(validation_f isGoalState) {
+    state_t currentState;
+    cost_t currentCost, newCost;
+    currentCost = _initialCost;
+    trace_state<state_t> *traceState;
+    list<state_t> passed, solution;
+    list<pair<cost_t, trace_state<state_t> *>> waiting;
+    container_t<state_t> containedSolution;
+
+    // Generate a set of cost and trace state to find the lowest cost aka where to go next
+    waiting.push_back(make_pair(currentCost, new trace_state<state_t>{nullptr, _initialState}));
+
+    while(!waiting.empty()){
+        // Prepare to go to the next state, which is next in the queue
+        currentState = waiting.front().second->self;
+        currentCost = waiting.front().first; // First element of pair is cost
+        traceState = waiting.front().second; // Second element is trace state
+        waiting.pop_front();
+
+        if (isGoalState(currentState)) {
+            while (traceState->parent != nullptr) {
+                // Add stack trace to the solution list
+                solution.push_front(traceState->self);
+                traceState = traceState->parent;
+            }
+
+            // Add self trace to solution list
+            solution.push_front(traceState->self);
+
+
+            // Convert to a vector
+            for (state_t &st: solution) {
+                containedSolution.push_back(st);
+            }
+
+            // Return early if a goal state was found
+            return containedSolution;
+        }
+
+        // Check if current state has already been passed otherwise push it
+        if(!(find(passed.begin(), passed.end(), currentState) != passed.end())) {
+            passed.push_back(currentState);
+            auto transitions = _transitionFunction(currentState);
+
+            for (auto transition: transitions) {
+                auto successor{currentState};
+                transition(successor);
+
+                if (!_invariantFunction(successor)) {
+                    continue;
+                }
+                newCost = _costFunction(successor, currentCost);
+                waiting.push_back(make_pair(newCost, new trace_state<state_t>{traceState, successor}));
+            }
+            if (!transitions.empty()) {
+                // Sort the list to make sure that the lowest cost is first.
+                waiting.sort([](const pair<cost_t, trace_state<state_t> *> &a,
+                             pair<cost_t, trace_state<state_t> *> &b) { return a.first < b.first; });
+            }
+
+        }
+    }
+
+    return containedSolution;
+}
 
 #endif //PUZZLEENGINE_REACHABILITY_HPP
